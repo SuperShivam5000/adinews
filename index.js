a8c1b82cabcfe0b6a9d110038623862ac44f09ca
Aditya Tiwari <adityatiwari2003.at@gmail.com>
2025-03-16 17:27:02 +0530

    v3

--- index.js
+++ index.js
@@ -55,4 +55,4 @@ app.post('/newsfinder', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server running on port 3000'));
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
